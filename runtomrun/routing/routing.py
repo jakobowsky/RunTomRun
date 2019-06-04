3d299d0781bf5a2de18896be3f0c2cd017a00e62
jakobowsky <jakob.wolitzki@gmail.com>
2019-06-04 13:56:13 +0200

    generating route

--- runtomrun/routing/routing.py
+++ runtomrun/routing/routing.py
@@ -1,2 +1,86 @@
-def check(x):
-    return (x+2)
+import geopy
+import geopy.distance
+import math
+# in meters
+RESOLUTION = 100
+RESOLUTION_DISTANCE = geopy.distance.geodesic(meters=RESOLUTION)
+
+NORTH = 0
+EAST = 90
+SOUTH = 180
+WEST = 270
+COMPASS = (NORTH, EAST, SOUTH, WEST)
+
+
+class Routes:
+
+    def __init__(self, lat: float, lng: float, length: int) -> None:
+        self.lat = lat
+        self.lng = lng
+        self.length = length
+        self.start = geopy.Point(lat, lng)
+        self.points_n = math.floor(self.length / RESOLUTION)
+
+    def get(self):
+        routes = []
+        # routes += self.__route_circle()
+        routes += self.__route_square()
+        return routes
+
+    def __route_circle(self):
+        r = self.length / (2 * math.pi)
+        r_distance = geopy.distance.geodesic(meters=r)
+        angle_d = 360 / self.points_n
+
+        routes = []
+        for direction in COMPASS:
+            center = r_distance.destination(point=self.start, bearing=direction)
+
+            points = []
+            for i in range(0, self.points_n):
+                p = r_distance.destination(point=center, bearing=angle_d*i)
+                points.append((p.latitude, p.longitude))
+            routes.append(points)
+
+        return routes
+
+    def __route_square(self):
+        a = self.length / 4
+        a_points = math.floor(self.points_n / 4)
+        result = []
+        last = self.start
+        compass_array = [[NORTH, EAST, SOUTH, WEST],
+                         [NORTH, WEST, SOUTH, EAST],
+                         [SOUTH, WEST, NORTH, EAST],
+                         [SOUTH, EAST, NORTH, WEST]]
+        for direction in compass_array:
+            points = []
+            for destination in direction:
+                for i in range(1, a_points):
+                    p = RESOLUTION_DISTANCE.destination(point=last, bearing=destination)
+                    points.append((p.latitude, p.longitude))
+                    last = p
+            result.append(points)
+
+        return result
+        # a_distance = a / (self.points_n/4)
+        # r_distance = geopy.distance.geodesic(meters=a_distance)
+        # center = r_distance.destination(point=self.start, bearing=0)
+        # points = []
+        # for i in range(0, self.points_n):
+        #     p = r_distance.destination(point=center)
+
+
+def pr(routes: list):
+    for r in routes:
+        for p in r:
+            print(str(p[0]) + "\t" + str(p[1]))
+        print("\n\n")
+
+def generate_route(length, lat, lng):
+    length = 7000
+    lat = 52.403596
+    lng = 16.950054
+    result = Routes(lat, lng, length).get()
+    # result to tablica która leci do mnie
+    return result
