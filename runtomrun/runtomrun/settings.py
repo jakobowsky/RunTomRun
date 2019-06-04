fdf355d91ff9db49e355b9f1ab266f4be31d8f84
jakobowsky <jakob.wolitzki@gmail.com>
2019-06-04 14:40:21 +0200

    setings

--- runtomrun/runtomrun/settings.py
+++ runtomrun/runtomrun/settings.py
@@ -139,4 +139,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR)
 STATIC_URL = '/static/'
